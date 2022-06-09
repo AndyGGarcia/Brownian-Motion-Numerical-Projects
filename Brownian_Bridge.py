7272d410594981320886219551438bccdaba5f6e
AndyGGarcia <92745245+AndyGGarcia@users.noreply.github.com>
2022-06-09 00:04:52 -0400

    Update Brownian_Bridge.py
    Signed-off-by: AndyGGarcia <92745245+AndyGGarcia@users.noreply.github.com>

--- Brownian_Bridge.py
+++ Brownian_Bridge.py
@@ -1,12 +1,11 @@
-'''Generating Brownian bridge off t = 100 and dt step == 1'''
+'''Generating Brownian bridge off t = 1 and dt step = an input by user'''
 
 import random
-from threading import local
 import numpy as np
 import matplotlib.pyplot as plt
 def Standard_B_Path(t, dt):
     """
-    Returns a Brownian path of length t, with steps of size dt.
+    Returns a Standard Brownian path of length t, with steps of size dt.
     """
     if t == 0:
         return [0]
@@ -31,8 +30,13 @@ def BrownianBridge(t,dt):
     B_path[-1]=0
     return B_path
 
-#interval = float(input("Enter the time interval t value: ")) will always be 1 on brownian bridge
-step_size = float(input("Enter the step size value: "))
+#interval = float(input("Enter the time interval t value: ")) interval will always be [0,1] on brownian bridge so we dont take input
+"""
+Generates 100 Brownian Bridges and graphs them
+"""
+step_size = float(input("Enter the step size value: "))   #step_size determines how many intervals our brownian bridge will have
+num_steps = int(1/step_size)
+x_axis = np.linspace(0,1,num_steps) #generating an array of evenly spaced numbers on an interval to use as x axis on graph
 for i in range(100):
-    plt.plot(BrownianBridge(1, step_size))
+    plt.plot(x_axis,BrownianBridge(1, step_size))
 plt.show()
