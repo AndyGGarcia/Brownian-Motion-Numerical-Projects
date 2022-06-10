8155ade4cee41ee198a71e2bd548ca89baf9f1ac
AndyGGarcia <92745245+AndyGGarcia@users.noreply.github.com>
2022-06-09 21:00:09 -0400

    Update ABrownianMotion.py
    Signed-off-by: AndyGGarcia <92745245+AndyGGarcia@users.noreply.github.com>

--- ABrownianMotion.py
+++ ABrownianMotion.py
@@ -20,6 +20,11 @@ def Brownian_Path(t, dt):
         path.append(path[i-1] +  np.random.normal(0,np.sqrt(local_t- (local_t-dt))))
         time.append(local_t)
     return path
+"""
+Our brownian path function generates brownian paths for a given time interval [0,t] with step size dt to determine increment size. 
+Our program takes input from user for an interval parameter t, step size dt and a number of paths num_paths, to generate a num_paths amount 
+of brownian paths and then returning a graph of these paths 
+"""
 interval = float(input("Enter the time interval t value: "))
 step_size = float(input("Enter the step size value: "))
 num_paths = int(input("Enter the number of paths you would like to simulate: "))
